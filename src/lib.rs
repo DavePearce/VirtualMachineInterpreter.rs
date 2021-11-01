21049db111c8293ad555c0adb349610e6a3a2faa
DavePearce <david.pearce@ecs.vuw.ac.nz>
2021-11-01 13:27:18 +1300

    Lots and lots of stuff!!
    Its starting to take some very useful shape.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,4 @@
 use num::BigUint;
-// instruction format
-
-// --> opcodes / operands
-//
-// operand
-// --> immediate
-// --> register?
-// --> ???
 
 /// Used for converting a given domain into a physical count of
 /// elements in that domain.  For example, the domain of 2bits would
@@ -14,7 +6,7 @@ use num::BigUint;
 /// distinct elements in the domain).
 trait DomainSize {
     /// Calculate the number of elements in the given domain.
-    fn to_domain_size(&self) -> BigUint;
+    fn to_domsize(&self) -> BigUint;
 }
 
 // ================================================================
@@ -35,7 +27,7 @@ impl From<u8> for Bits {
 }
 
 impl DomainSize for Bits {
-    fn to_domain_size(&self) -> BigUint {
+    fn to_domsize(&self) -> BigUint {
 	let mut count = BigUint::from(1u32);
 	//
 	for i in 0 .. self.value {
@@ -52,17 +44,19 @@ impl DomainSize for Bits {
 
 #[derive(Clone,Copy,PartialEq)]
 struct Bytes {
+    // INVARIANT: value > 0    
     value : u8,
 }
 
 impl From<u8> for Bytes {
     fn from(value:u8) -> Self {
+	assert!(value != 0);
 	Bytes{value}
     }
 }
 
 impl DomainSize for Bytes {
-    fn to_domain_size(&self) -> BigUint {
+    fn to_domsize(&self) -> BigUint {
 	let mut count = BigUint::from(1u32);
 	//
 	for i in 0 .. self.value {
@@ -93,6 +87,7 @@ impl DomainSize for Bytes {
 /// includes two three-bit operands, and a two bit opcode.  This means
 /// we can have at most four instructions in this class, and each
 /// operand can take on eight distinct values.
+#[derive(PartialEq)]
 struct Format {
     /// Determines the overall width (in bytes) of an instruction in
     /// this class.  Generally speaking, virtual machines normally
@@ -102,6 +97,8 @@ struct Format {
     /// instructions (e.g. 16bit instructions, and 32bit "double"
     /// instructions).
     width: Bytes,
+    /// Human-readable label for this format
+    label: String,
     /// Determine the number of distinct instructions in this class.
     opcode : Bits,
     /// Determine the number and size of operands for all instructions
@@ -110,14 +107,162 @@ struct Format {
 }
 
 impl Format {
-    pub fn new(width:Bytes, opcode: Bits, operands: Vec<Bits>) -> Format {	
-	Format{width,opcode,operands}
+    pub fn new(width:Bytes, label: &str, opcode: Bits, operands: Vec<Bits>) -> Format {	
+	let r = Format{width,label:label.to_string(),opcode,operands};
+	// Sanity check there is enough space
+	assert!(width.to_domsize() >= r.to_domsize());
+	//
+	r
     }
 }
 
-// instruction semantics
+impl DomainSize for Format {
+    fn to_domsize(&self) -> BigUint {
+	let mut count = BigUint::from(self.opcode.to_domsize());
+	//
+	for op in &self.operands {
+	    count = count * op.to_domsize();
+	}
+	//
+	count
+    }
+}
 
-// machine state
+// =====================================================
+// (Random Access) Memory
+// =====================================================
+
+/// Describes a fixed-size array of bytes.
+struct Memory<'a> {
+    contents: &'a mut [u8]
+}
+
+impl<'a> Memory<'a> {
+    pub fn new(contents: &'a mut [u8]) -> Self {
+	Memory{contents}
+    }
+    pub fn read_u8(&self, address : usize) -> u8 {
+	self.contents[address]
+    }
+    pub fn read_u16(&self, address : usize) -> u16 {
+	todo!("implement me");
+    }
+    pub fn write_u8(&mut self, address : usize, value: u8) {
+	self.contents[address] = value; 
+    }
+    pub fn write_u16(&mut self, address : usize, value: u16) {
+	todo!("implement me");	
+    }    
+}
+
+// =====================================================
+// Machine Codes
+// =====================================================
+
+/// Microcode is used to define the semantics of virtual machine
+/// instructions.  This means, for example, they can be executed using
+/// a "virtual machine interpreter".
+#[derive(Clone,Copy,PartialEq)]
+enum MachineCode {
+    /// x := y (8 bits)
+    COPY_8(usize,usize),
+    /// x := y (16 bits)
+    COPY_16(usize,usize),
+    /// pc := i
+    GOTO(usize),
+    /// pc := pc + i
+    JUMP(usize),
+    /// x := i
+    LOAD_8(usize,u8)
+}
+
+// =====================================================
+// Machine State
+// =====================================================
+
+struct MachineState<'a> {
+    /// Program counter.  This determines where in the instruction
+    /// memory the machine is currently executing.  The program
+    /// counter always points to the *next* instruction to be
+    /// executed.
+    pc: usize,
+    /// Available memory
+    data: Memory<'a>,
+}
+
+impl<'a> MachineState<'a> {
+    pub fn new(pc: usize, bytes: &'a mut [u8]) -> Self {
+	MachineState{pc,data: Memory::new(bytes)}
+    }
+    pub fn execute(&mut self, insn: MachineCode) {
+	match insn {
+	    MachineCode::COPY_8(x,y) => {
+		let v = self.data.read_u8(y);
+		self.data.write_u8(x,v);
+		self.pc += 1;
+	    }
+	    MachineCode::COPY_16(x,y) => {
+		let v = self.data.read_u16(y);
+		self.data.write_u16(x,v);
+		self.pc += 1;
+	    }
+	    MachineCode::GOTO(i) => {
+		self.pc = i;
+	    }
+	    MachineCode::JUMP(i) => {
+		self.pc += i;
+	    }	    
+	    MachineCode::LOAD_8(x,i) => {
+		self.data.write_u8(x,i);
+		self.pc += 1;
+	    }
+	    _ => {
+		todo!("Implement more instructions")
+	    }
+	}
+    }
+}
+
+
+// =====================================================
+// Instruction
+// =====================================================
+
+struct Instruction<'a> {
+    /// Mnemonic for referring to the instruction.  Every instruction
+    /// should have a unique mnemonic.
+    mnemonic: &'a str,
+    /// Format associated with this instruction.
+    format: &'a Format,
+    /// Machine semantics associated with instruction.
+    semantic: &'a [MachineCode]
+}
+
+impl<'a> Instruction<'a> {
+    pub fn new(mnemonic: &'a str, format: &'a Format, semantic: &'a [MachineCode]) -> Self {
+	Instruction{mnemonic,format,semantic}
+    }
+
+    /// Apply a given instruction to a given machine state.
+    pub fn execute(&self, state: &mut MachineState) {
+	for insn in self.semantic {
+	    state.execute(*insn);
+	}
+    }
+}
+
+// =====================================================
+// Instruction Set
+// =====================================================   
+
+/// A collection of instructions.
+struct InstructionSet<'a> {
+    insns : Vec<Instruction<'a>>
+}
+
+// =====================================================
+// Tests
+// =====================================================   
 
 #[cfg(test)]
 mod tests {
@@ -126,59 +271,144 @@ mod tests {
     use crate::Bytes;    
     use crate::Format;
     use crate::DomainSize;
+    use crate::MachineCode;
+    use crate::MachineState;
+    use crate::Memory;    
 
     // =====================================================
     // Bits
     // =====================================================   
-    
+
+    #[test]
+    #[should_panic]
+    fn test_bites_00() {
+	let _b = Bits::from(0);
+    }    
+
     #[test]
     fn test_bits_01() {
 	let b = Bits::from(1);
-	assert_eq!(b.to_domain_size(),BigUint::from(2u32));
+	assert_eq!(b.to_domsize(),BigUint::from(2u32));
     }
 
     #[test]
     fn test_bits_02() {
 	let b = Bits::from(2);
-	assert_eq!(b.to_domain_size(),BigUint::from(4u32));
+	assert_eq!(b.to_domsize(),BigUint::from(4u32));
     }
 
     #[test]
     fn test_bits_03() {
 	let b = Bits::from(3);
-	assert_eq!(b.to_domain_size(),BigUint::from(8u32));
+	assert_eq!(b.to_domsize(),BigUint::from(8u32));
     }
 
     // =====================================================
     // Bytes
     // =====================================================   
+
+    #[test]
+    #[should_panic]
+    fn test_bytes_00() {
+	let _b = Bytes::from(0);
+    }    
     
     #[test]
     fn test_bytes_01() {
 	let b = Bytes::from(1);
-	assert_eq!(b.to_domain_size(),BigUint::from(256u32));
+	assert_eq!(b.to_domsize(),BigUint::from(256u32));
     }
 
     #[test]
     fn test_bytes_02() {
 	let b = Bytes::from(2);
-	assert_eq!(b.to_domain_size(),BigUint::from(65536u32));
+	assert_eq!(b.to_domsize(),BigUint::from(65536u32));
     }
 
     #[test]
     fn test_bytes_03() {
 	let b = Bytes::from(3);
-	assert_eq!(b.to_domain_size(),BigUint::from(16777216u32));
+	assert_eq!(b.to_domsize(),BigUint::from(16777216u32));
     }
 
     // =====================================================
     // Formats
     // =====================================================   
+
+    #[test]
+    #[should_panic]
+    fn test_format_01() {
+	// Check that 10bits does not fit into one byte
+	let width = Bytes::from(1);
+	let opcode = Bits::from(10);
+        Format::new(width,"fmt",opcode,vec![]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_format_02() {
+	// Check that 10bits does not fit into one byte
+	let width = Bytes::from(1);
+	let opcode = Bits::from(4);
+	let operand = Bits::from(3);
+        Format::new(width,"fmt",opcode,vec![operand,operand]);
+    }   
     
     #[test]
-    fn test() {
-	let op4 : Bits = Bits::from(4);
-	let b1 : Bytes = Bytes::from(1);	
-        let fmt_rr = Format::new(b1,op4,vec![op4]);
+    fn test_format_03() {
+	// Check that 8 bits fits into one byte
+	let width = Bytes::from(1);
+	let opcode = Bits::from(4);
+	let operand = Bits::from(4);
+        let fmt = Format::new(width,"fmt",opcode,vec![operand]);
+	assert_eq!(fmt.to_domsize(),BigUint::from(256u32));
     }
+
+    #[test]
+    fn test_format_04() {
+	// Check that 8 bits fits into one byte
+	let width : Bytes = Bytes::from(1);	
+	let opcode : Bits = Bits::from(4);
+	let operand : Bits = Bits::from(2);
+        let fmt = Format::new(width,"fmt",opcode,vec![operand,operand]);
+	assert_eq!(fmt.to_domsize(),BigUint::from(256u32));	
+    }
+
+    #[test]
+    fn test_format_05() {
+	// Check that 6 bits fit into one byte with spare
+	let width : Bytes = Bytes::from(1);	
+	let opcode : Bits = Bits::from(6);
+        let fmt = Format::new(width,"fmt",opcode,vec![]);
+	assert_eq!(fmt.to_domsize(),BigUint::from(64u32));	
+    }
+    
+    #[test]
+    fn test_format_06() {
+	// Check that 6 bits fits into one byte with space
+	let width : Bytes = Bytes::from(1);	
+	let opcode : Bits = Bits::from(2);
+	let operand : Bits = Bits::from(2);
+        let fmt = Format::new(width,"fmt",opcode,vec![operand,operand]);
+	assert_eq!(fmt.to_domsize(),BigUint::from(64u32));	
+    }
+
+    // =====================================================
+    // Machine Codes
+    // =====================================================   
+
+    #[test]
+    fn test_microcode_01() {
+	let mut bytes : [u8;1] = [0];
+	let mut state = MachineState::new(0,&mut bytes);
+	//
+	state.execute(MachineCode::LOAD_8(0,1));
+	//
+	assert_eq!(bytes[0],1);
+    }
+    
+    // =====================================================
+    // Machine Codes
+    // =====================================================   
+   
 }
